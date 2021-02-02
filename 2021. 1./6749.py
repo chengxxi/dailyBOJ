622ccc860322ca7ef9747cb5fefed3fac568fde8
chengxxi <heeya121z@naver.com>
2021-02-02 21:06:49 +0900

    20210129

---
+++ 2021. 1./6749.py
@@ -0,0 +1,15 @@
+# 6749: Next in line
+
+youngest = int(input())
+middle = int(input())
+oldest = middle + (middle - youngest)
+print(oldest)
+
+"""
+You know a family with three children. 
+Their ages form an arithmetic sequence: the difference in ages between the middle child and youngest child 
+is the same as the difference in ages between the oldest child and the middle child. 
+For example, their ages could be 5, 10 and 15, since both adjacent pairs have a difference of 5 years.
+
+Given the ages of the youngest and middle children, what is the age of the oldest child?
+"""
