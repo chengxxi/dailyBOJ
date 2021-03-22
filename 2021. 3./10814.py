34c9468259fef609920ead5b398973406aa257ab
Seunghee Han <71359467+chengxxi@users.noreply.github.com>
2021-03-22 19:00:48 +0900

    20210322

---
+++ 2021. 3./10814.py
@@ -0,0 +1,23 @@
+# 10814: 나이순 정렬
+
+N = int(input())
+members = []
+for _ in range(N):
+    m = list(input().split())
+    members.append(m)
+
+# lambda
+members.sort(key=lambda x: int(x[0]))
+# for i in range(N):
+#     print(members[i][0], members[i][1])
+
+
+for m in members:
+    print(*m)
+
+
+
+"""
+온라인 저지에 가입한 사람들의 나이와 이름이 가입한 순서대로 주어진다. 
+이때, 회원들을 나이가 증가하는 순으로, 나이가 같으면 먼저 가입한 사람이 앞에 오는 순서로 정렬하는 프로그램을 작성하시오.
+"""
